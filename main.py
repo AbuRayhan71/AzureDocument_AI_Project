8bf5ac734b96fe34cd772e430bd7505b36c7fe1f
MD ABU RAYHAN <aburayhanisar71@gmail.com>
2024-01-25 11:06:08 +1100

    layout

---
+++ main.py
@@ -0,0 +1,51 @@
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.formrecognizer import DocumentAnalysisClient
+
+endpoint = "https://documentintelligenceinterninstance.cognitiveservices.azure.com/"
+key = ""
+formUrl = "https://testing-ecal-publisher-assets.s3.amazonaws.com/intern_ai/2023-Fixture-Rounds-1-23.pdf"
+
+document_analysis_client = DocumentAnalysisClient(
+    endpoint=endpoint, credential=AzureKeyCredential(key)
+)
+
+poller = document_analysis_client.begin_analyze_document("prebuilt-layout", formUrl)
+result = poller.result()
+
+for idx, style in enumerate(result.styles):
+    print(
+        "Document contains {} content".format(
+            "handwritten" if style.is_handwritten else "no handwritten"
+        )
+    )
+
+for page in result.pages:
+    for line_idx, line in enumerate(page.lines):
+        print(
+            "...Line # {} has text content '{}'".format(
+                line_idx, line.content.encode("utf-8")
+            )
+        )
+
+    for selection_mark in page.selection_marks:
+        print(
+            "...Selection mark is '{}' and has a confidence of {}".format(
+                selection_mark.state, selection_mark.confidence
+            )
+        )
+
+for table_idx, table in enumerate(result.tables):
+    print(
+        "Table # {} has {} rows and {} columns".format(
+            table_idx, table.row_count, table.column_count
+        )
+    )
+
+    for cell in table.cells:
+        print(
+            "...Cell[{}][{}] has content '{}'".format(
+                cell.row_index, cell.column_index, cell.content.encode("utf-8"),
+            )
+        )
+
+print("----------------------------------------")
